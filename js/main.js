a8a6a4467b65788c72a152810c2e1edf1ac238a1
Aleksandr-lit <53788620+Aleksandr-lit@users.noreply.github.com>
2021-08-21 12:00:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,63 @@
+"use strict"
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+        // document.body.classList.toggle('_lock');
+    });
+}
+
+new Swiper ('.banners__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+		dynamicBullets: false,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    }
+});
+
+new Swiper ('.products__slider', {
+    pagination: {
+        el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+		dynamicBullets: false,
+    },
+    slidesPerView: 3,
+    loop: true,
+    // autoplay: {
+    //     delay: 3500,
+    //     disableOnInteraction: false,
+    // }
+});
+
+$('.menu__link').click(function (event) {
+    event.preventDefault();
+    $(this).siblings('.menu__sub-list').toggle(200);
+    $(this).parent().toggleClass('active');
+  });
+
+fetch('https://www.wona.co.il/test_8192735.php', { mode: 'no-cors'})
+    
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+
+    })
+    .catch(function () {
+
+    });
+
